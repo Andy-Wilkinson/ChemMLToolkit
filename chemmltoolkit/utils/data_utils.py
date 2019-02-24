6ae67df3edc4bb1599b38efe1a3e2c8886539b5f
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2019-02-24 14:57:16 +0000

    Add installation script for RDKit

---
+++ chemmltoolkit/utils/data_utils.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+import urllib3
+import tarfile
+import zipfile
+
+http = urllib3.PoolManager()
+
+
+def get_file(filename,
+             url,
+             cache_subdir='downloads',
+             cache_dir=None):
+
+    if cache_dir is None:
+        cache_dir = os.path.join(os.path.expanduser('~'), '.chemmltoolkit')
+
+    datadir_base = os.path.expanduser(cache_dir)
+    datadir = os.path.join(datadir_base, cache_subdir)
+    fpath = os.path.join(datadir, filename)
+
+    if not os.path.exists(datadir):
+        os.makedirs(datadir)
+
+    if not os.path.exists(fpath):
+        print('Downloading data from', url)
+
+        with http.request('GET', url, preload_content=False) as r, \
+                open(fpath, 'wb') as out_file:
+            shutil.copyfileobj(r, out_file)
+
+    return fpath
+
+
+def extract_all(archive,
+                cache_subdir=None,
+                cache_dir=None):
+
+    if cache_dir is None:
+        cache_dir = os.path.join(os.path.expanduser('~'), '.chemmltoolkit')
+
+    if cache_subdir is None:
+        archive_name = os.path.basename(archive).split(os.path.extsep)[0]
+        cache_subdir = os.path.join('downloads', archive_name)
+
+    datadir_base = os.path.expanduser(cache_dir)
+    datadir = os.path.join(datadir_base, cache_subdir)
+
+    if not os.path.exists(datadir):
+        print('Extracting datafile', archive)
+        os.makedirs(datadir)
+
+        if archive.endswith(".tar.bz2"):
+            with tarfile.open(archive, "r:bz2") as tar:
+                tar.extractall(path=datadir)
+        elif archive.endswith(".zip"):
+            with zipfile.ZipFile(archive) as zfile:
+                zfile.extractall(path=datadir)
+
+    return datadir
---
+++ chemmltoolkit/utils/install_utils.py
@@ -0,0 +1,47 @@
+import os
+import glob
+import shutil
+import importlib
+
+from chemmltoolkit.utils.data_utils import get_file, extract_all
+
+
+def install_rdkit():
+    """Installs RDKit from the conda package.
+
+    This function will download the RDKit conda package, extract the files
+    and copy them to the relevant locations. This is useful for situations
+    where the conda tool is not available (e.g. Google Colab)
+    """
+    if importlib.util.find_spec('rdkit'):
+        import rdkit
+        print(f'RDKit version {rdkit.__version__} already installed')
+    else:
+        # Download and extract the package
+
+        rdkit_package_filename = 'rdkit.tar.bz2'
+        rdkit_package_url = 'https://anaconda.org/rdkit/rdkit/2018.09.1.0/' + \
+            'download/linux-64/rdkit-2018.09.1.0-py36h71b666b_1.tar.bz2'
+
+        rdkit_package_archive = get_file(
+            rdkit_package_filename, rdkit_package_url)
+        rdkit_package_dir = extract_all(rdkit_package_archive)
+
+        # Copy the libraries to the relevant locations
+
+        shutil.copytree(os.path.join(rdkit_package_dir,
+                                     'lib/python3.6/site-packages/rdkit'),
+                        '/usr/local/lib/python3.6/dist-packages/rdkit')
+
+        so_files = glob.glob(os.path.join(rdkit_package_dir, 'lib/*.so.*'))
+        for filename in so_files:
+            shutil.copy(filename, '/usr/lib/x86_64-linux-gnu/')
+
+        # Create a symbolic link for the libboost_python library as rdkit is
+        # compiled with a slightly different naming system in Google Colab
+
+        os.symlink('/usr/lib/x86_64-linux-gnu/libboost_python3-py36.so.1.65.1',
+                   '/usr/lib/x86_64-linux-gnu/libboost_python3.so.1.65.1')
+
+        import rdkit
+        print(f'RDKit version {rdkit.__version__} installed successfully')
