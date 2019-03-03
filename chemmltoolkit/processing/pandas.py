7abee7313c9feab0bbd31375e5d50550d20d9e3b
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2019-03-03 21:02:36 +0000

    Helper method to add pandas molecule column

--- chemmltoolkit/processing/pandas.py
+++ chemmltoolkit/processing/pandas.py
@@ -1,5 +1,17 @@
 import pandas as pd
 from rdkit import Chem
+from rdkit.Chem import PandasTools
+from chemmltoolkit.processing.mols import MolCleaner
+
+
+def df_addmoleculecolumn(df, smilesCol='Smiles', molCol='ROMol', clean=False):
+    PandasTools.AddMoleculeColumnToFrame(df, smilesCol, molCol)
+
+    if clean:
+        cleaner = MolCleaner()
+        df[molCol] = df[molCol].apply(cleaner.clean_mol)
+
+    return df
 
 
 def df_addfingerprint_morgan(df, molCol='ROMol', prefix='M_', radius=2):
@@ -9,6 +21,8 @@ def df_addfingerprint_morgan(df, molCol='ROMol', prefix='M_', radius=2):
     return df.join(df[molCol].apply(generate_fingerprint).add_prefix(prefix))
 
 
+pd.DataFrame.addmoleculecolumn = df_addmoleculecolumn
 pd.DataFrame.addfingerprint_morgan = df_addfingerprint_morgan
 
+del(df_addmoleculecolumn)
 del(df_addfingerprint_morgan)
