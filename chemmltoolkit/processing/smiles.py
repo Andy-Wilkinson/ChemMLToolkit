dce782aad63bfb1f101c254511816a697eda8b4e
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2019-03-09 18:04:13 +0000

    Functions for tokenising SMILES strings

--- chemmltoolkit/processing/smiles.py
+++ chemmltoolkit/processing/smiles.py
@@ -15,3 +15,62 @@ def desalt_smiles(smiles: str) -> str:
     """
     components = smiles.split('.')
     return max(components, key=len)
+
+
+def split_smiles(smiles: str) -> list:
+    """Splits a SMILES string into individual tokens.
+
+    This will split a SMILES string into tokens representing
+    individual parts of the SMILES string. Each individual token
+    will be one of the following,
+
+    - A single atom (including hydrogens or charge if specified)
+    - A ring connection number (single digit or multi-digit)
+    - Another SMILES character
+
+    Args:
+        smiles: The input SMILES string.
+
+    Returns:
+        A list of tokens.
+    """
+    result = []
+    while smiles != '':
+        if smiles[:2] == 'Cl' or smiles[:2] == 'Br':
+            token_length = 2
+        elif smiles[0] == '%':  # Ring linkage numbers >9 are prefixed by '%'
+            token_length = 3
+        elif smiles[0] == '[':
+            token_length = smiles.index(']') + 1
+        else:
+            token_length = 1
+
+        result.append(smiles[:token_length])
+        smiles = smiles[token_length:]
+    return result
+
+
+def get_token_list(max_ring_number=9, chiral_carbon=True) -> list:
+    tokens = []
+
+    # SMILES 'organic' subset that do not need '[...]'
+    tokens += ['B', 'C', 'N', 'O', 'P', 'S', 'F', 'Cl', 'Br', 'I']
+    tokens += ['c', 'n', 'o', 's']
+    tokens += ['[nH]', '[n+]', '[N+]', '[O-]']
+
+    # Chiral atoms
+    if chiral_carbon:
+        tokens += ['[C@]', '[C@@]', '[C@H]', '[C@@H]']
+
+    # Control tokens
+    tokens += [
+        '-', '=', '#',  # Bonds
+        '(', ')',  # Atom groups
+        '/', '\\',  # Double bond geometry
+    ]
+
+    # Ring tokens
+    tokens += [f'{x}' for x in range(1, min(max_ring_number, 9) + 1)] \
+        + [f'%{x}' for x in range(10, max_ring_number + 1)]
+
+    return tokens
--- tests/processing/test_smiles.py
+++ tests/processing/test_smiles.py
@@ -12,3 +12,60 @@ class TestSmiles(object):
     def test_desalt_smiles(self, smiles_full, smiles_desalted):
         smiles_result = smiles.desalt_smiles(smiles_full)
         assert smiles_result == smiles_desalted
+
+    @pytest.mark.parametrize("smiles_input,smiles_tokens", [
+        ('CCCN', ['C', 'C', 'C', 'N']),  # Simple example
+        ('C=C', ['C', '=', 'C']),  # Bond characters
+        ('CN(C)C', ['C', 'N', '(', 'C', ')', 'C']),  # Braces
+        ('ClCCBr', ['Cl', 'C', 'C', 'Br']),  # Two-character atoms
+        ('O[Pt]O', ['O', '[Pt]', 'O']),  # [xxx] atoms
+        ('C1CC1', ['C', '1', 'C', 'C', '1']),  # Ring numbering (<=9)
+        ('C%10CC%10', ['C', '%10', 'C', 'C', '%10']),  # Ring numbering (>9)
+    ])
+    def test_split_smiles(self, smiles_input, smiles_tokens):
+        result = smiles.split_smiles(smiles_input)
+        assert result == smiles_tokens
+
+    tokens_atoms = ['B', 'C', 'N', 'O', 'P', 'S', 'F', 'Cl', 'Br', 'I',
+                    'c', 'n', 'o', 's', '[nH]', '[n+]', '[N+]', '[O-]']
+    tokens_chiralC = ['[C@]', '[C@@]', '[C@H]', '[C@@H]']
+    tokens_control = ['-', '=', '#', '(', ')', '/', '\\']
+
+    def test_get_token_list(self):
+        token_list = smiles.get_token_list()
+        expected = self.tokens_atoms \
+            + self.tokens_chiralC \
+            + self.tokens_control \
+            + ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        assert token_list == expected
+
+    def test_get_token_list_ringcount0(self):
+        token_list = smiles.get_token_list(max_ring_number=0)
+        expected = self.tokens_atoms \
+            + self.tokens_chiralC \
+            + self.tokens_control
+        assert token_list == expected
+
+    def test_get_token_list_ringcount5(self):
+        token_list = smiles.get_token_list(max_ring_number=5)
+        expected = self.tokens_atoms \
+            + self.tokens_chiralC \
+            + self.tokens_control \
+            + ['1', '2', '3', '4', '5']
+        assert token_list == expected
+
+    def test_get_token_list_ringcount12(self):
+        token_list = smiles.get_token_list(max_ring_number=12)
+        expected = self.tokens_atoms \
+            + self.tokens_chiralC \
+            + self.tokens_control \
+            + ['1', '2', '3', '4', '5', '6', '7', '8', '9'] \
+            + ['%10', '%11', '%12']
+        assert token_list == expected
+
+    def test_get_token_list_nochiralC(self):
+        token_list = smiles.get_token_list(chiral_carbon=False)
+        expected = self.tokens_atoms \
+            + self.tokens_control \
+            + ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        assert token_list == expected
