d1b6818343b85bf7ac0bae301c4508890c037600
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2019-08-20 01:06:14 +0100

    Add simple adjacency matrix operations

---
+++ chemmltoolkit/processing/adjacency_ops.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+def add_master_node(adjacency_matrices):
+    """Adds master nodes to adjacency matricies
+
+    This will add a master node (an imaginary node that connects to all other
+    nodes) to a set of adjacency matricies. The size of each adjacency matrix
+    will hence increase by one in each dimension. Note that the master node
+    is not connected to itself.
+
+    Args:
+        adjacency_matrices: The input list of adjacency matricies.
+
+    Returns:
+        The resulting list of adjacency matricies.
+    """
+    adj = np.pad(adjacency_matrices,
+                 ((0, 0), (0, 1), (0, 1)),
+                 mode='constant',
+                 constant_values=1)
+    adj[:, adj.shape[1]-1, adj.shape[2]-1] = 0
+    return adj
+
+
+def add_self_loops(adjacency_matrices):
+    """Adds self loops to adjacency matricies
+
+    This will add a self loops (connections between every node and itself) to
+    a set of adjacency matricies.
+
+    Args:
+        adjacency_matrices: The input list of adjacency matricies.
+
+    Returns:
+        The resulting list of adjacency matricies.
+    """
+    adj = adjacency_matrices.copy()
+    mask = np.eye(adj.shape[1], dtype=bool)
+    adj[:, mask] = 1.0
+    return adj
+
+
+def normalise(adjacency_matrices):
+    """Normalises adjacency matricies
+
+    This will normalise (scale all rows to sum to one) a set of
+    adjacency matricies.
+
+    Args:
+        adjacency_matrices: The input list of adjacency matricies.
+
+    Returns:
+        The resulting list of adjacency matricies.
+    """
+    def normalise_matrix(matrix):
+        degree = np.diag(np.sum(matrix, axis=0))
+        return np.linalg.inv(degree) @ matrix
+
+    adjs = [normalise_matrix(matrix) for matrix in adjacency_matrices]
+    return np.array(adjs)
---
+++ tests/processing/test_adjacency_ops.py
@@ -0,0 +1,54 @@
+import pytest
+import numpy as np
+import chemmltoolkit.processing.adjacency_ops as adjacency_ops
+
+
+class TestAdjacencyOps(object):
+    @pytest.mark.parametrize("input,expected_result", [
+        ([
+            [[0, 1, 0], [1, 1, 0], [0, 1, 1]],
+            [[1, 2, 3], [2, 3, 4], [3, 4, 5]],
+        ], [
+            [[0, 1, 0, 1], [1, 1, 0, 1], [0, 1, 1, 1], [1, 1, 1, 0]],
+            [[1, 2, 3, 1], [2, 3, 4, 1], [3, 4, 5, 1], [1, 1, 1, 0]],
+        ]),
+    ])
+    def test_add_master_node(self, input, expected_result):
+        input = np.array(input)
+        reference_input = input.copy()
+        result = adjacency_ops.add_master_node(input)
+        assert (result == expected_result).all()
+        assert (input == reference_input).all()
+
+    @pytest.mark.parametrize("input,expected_result", [
+        ([
+            [[0, 1, 0], [1, 0, 0], [0, 1, 0]],
+            [[1, 2, 3], [2, 3, 4], [3, 4, 5]],
+        ], [
+            [[1, 1, 0], [1, 1, 0], [0, 1, 1]],
+            [[1, 2, 3], [2, 1, 4], [3, 4, 1]],
+        ]),
+    ])
+    def test_add_self_loops(self, input, expected_result):
+        input = np.array(input)
+        reference_input = input.copy()
+        result = adjacency_ops.add_self_loops(input)
+        assert (result == expected_result).all()
+        assert (input == reference_input).all()
+
+    @pytest.mark.parametrize("input,expected_result", [
+        ([
+            [[0, 0, 1], [0, 0, 1], [1, 1, 0]],
+            [[1, 2, 2], [2, 3, 5], [2, 5, 3]],
+        ], [
+            [[0, 0, 1], [0, 0, 1], [0.5, 0.5, 0]],
+            [[0.2, 0.4, 0.4], [0.2, 0.3, 0.5], [0.2, 0.5, 0.3]],
+        ]),
+    ])
+    def test_normalize(self, input, expected_result):
+        input = np.array(input)
+        reference_input = input.copy()
+        result = adjacency_ops.normalise(input)
+        result = np.round(result, 5)
+        assert (result == expected_result).all()
+        assert (input == reference_input).all()
