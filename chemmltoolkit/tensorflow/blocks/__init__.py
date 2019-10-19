4a32edc14737a5aa3a4cc2118fe35c6a43096da6
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2019-10-19 17:18:44 +0100

    Add Keras recurrent building block

--- chemmltoolkit/tensorflow/blocks/__init__.py
+++ chemmltoolkit/tensorflow/blocks/__init__.py
@@ -1,4 +1,5 @@
 from chemmltoolkit.tensorflow.blocks.conv1dBlock import Conv1DBlock
 from chemmltoolkit.tensorflow.blocks.denseBlock import DenseBlock
+from chemmltoolkit.tensorflow.blocks.recurrentBlock import RecurrentBlock
 
-__all__ = ['Conv1DBlock', 'DenseBlock']
+__all__ = ['Conv1DBlock', 'DenseBlock', 'RecurrentBlock']
