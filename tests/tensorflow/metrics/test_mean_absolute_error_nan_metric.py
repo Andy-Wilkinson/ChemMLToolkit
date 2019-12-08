67cf49e002d8f7a14ac41f5c7f15c7d295473fd5
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2019-12-08 20:41:04 +0000

    Give tests unique names for pytest

--- tests/tensorflow/metrics/test_mean_absolute_error_nan_metric.py
+++ tests/tensorflow/metrics/test_mean_absolute_error_nan_metric.py
@@ -1,9 +1,9 @@
 import numpy as np
 import tensorflow as tf
-from chemmltoolkit.tensorflow.metrics import RootMeanSquaredErrorNaN
+from chemmltoolkit.tensorflow.metrics import MeanAbsoluteErrorNaN
 
 
-class TestRootMeanSquaredErrorNaN(tf.test.TestCase):
+class TestMeanAbsoluteErrorNaNMetric(tf.test.TestCase):
     y_true = [
         [1.0, 2.0, 3.0, 4.0, 5.0],
         [6.0, 7.0, 8.0, 9.0, 10.0],
@@ -13,36 +13,35 @@ class TestRootMeanSquaredErrorNaN(tf.test.TestCase):
     ]
     y_pred = [
         [1.0, 2.0, 3.0, 4.0, 5.0],  # [0.0, ...]
-        [6.0, 8.0, 8.0, 7.0, 10.0],  # [0.0, 1.0, 0.0, 4.0, 0.0] = 1.0
+        [6.0, 8.0, 8.0, 7.5, 10.0],  # [0.0, 1.0, 0.0, 1.5, 0.0] = 0.5
         [1.0, 9.0, 2.0, 3.0, 9.0],  # [0.0, ...]
-        [3.0, 9.0, 5.0, 9.0, 9.0],  # [1.0, nan, nan, 9.0, nan] = 5.0
+        [3.0, 9.0, 5.0, 9.0, 9.0],  # [1.0, nan, nan, 3.0, nan] = 2.0
         [5.0, 4.0, 3.0, 2.0, 1.0]  # [nan, ...]
     ]
     sample_weight = [1.0, 2.0, 3.0, 4.0, 5.0]
-    expected_metric = np.sqrt(15.0 / 14.0)
-    expected_metric_weighted = np.sqrt(50.0 / 29.0)
-    expected_metric_per_sample = (np.sqrt(1.0) + np.sqrt(5.0)) / 4.0
-    expected_metric_per_sample_weighted = \
-        (2.0 * np.sqrt(1.0) + 4.0 * np.sqrt(5.0)) / 10.0
+    expected_metric = 6.5 / 14.0
+    expected_metric_weighted = 21.0 / 29.0
+    expected_metric_per_sample = 2.5 / 4.0
+    expected_metric_per_sample_weighted = 9.0 / 10.0
 
     def test_metric(self):
-        metric_obj = RootMeanSquaredErrorNaN()
+        metric_obj = MeanAbsoluteErrorNaN()
         metric = metric_obj(self.y_true, self.y_pred)
         self.assertAllClose(self.expected_metric, metric)
 
     def test_metric_weighted(self):
-        metric_obj = RootMeanSquaredErrorNaN()
+        metric_obj = MeanAbsoluteErrorNaN()
         metric = metric_obj(self.y_true, self.y_pred,
                             sample_weight=self.sample_weight)
         self.assertAllClose(self.expected_metric_weighted, metric)
 
     def test_metric_per_sample(self):
-        metric_obj = RootMeanSquaredErrorNaN(per_sample=True)
+        metric_obj = MeanAbsoluteErrorNaN(per_sample=True)
         metric = metric_obj(self.y_true, self.y_pred)
         self.assertAllClose(self.expected_metric_per_sample, metric)
 
     def test_metric_per_sample_weighted(self):
-        metric_obj = RootMeanSquaredErrorNaN(per_sample=True)
+        metric_obj = MeanAbsoluteErrorNaN(per_sample=True)
         metric = metric_obj(self.y_true, self.y_pred,
                             sample_weight=self.sample_weight)
         self.assertAllClose(self.expected_metric_per_sample_weighted, metric)
--- tests/tensorflow/metrics/test_root_mean_squared_error_nan_metric.py
+++ tests/tensorflow/metrics/test_root_mean_squared_error_nan_metric.py
@@ -1,9 +1,9 @@
 import numpy as np
 import tensorflow as tf
-from chemmltoolkit.tensorflow.metrics import MeanSquaredErrorNaN
+from chemmltoolkit.tensorflow.metrics import RootMeanSquaredErrorNaN
 
 
-class TestMeanSquaredErrorNaN(tf.test.TestCase):
+class TestRootMeanSquaredErrorNaNMetric(tf.test.TestCase):
     y_true = [
         [1.0, 2.0, 3.0, 4.0, 5.0],
         [6.0, 7.0, 8.0, 9.0, 10.0],
@@ -19,29 +19,30 @@ class TestMeanSquaredErrorNaN(tf.test.TestCase):
         [5.0, 4.0, 3.0, 2.0, 1.0]  # [nan, ...]
     ]
     sample_weight = [1.0, 2.0, 3.0, 4.0, 5.0]
-    expected_metric = 15.0 / 14.0
-    expected_metric_weighted = 50.0 / 29.0
-    expected_metric_per_sample = 6.0 / 4.0
-    expected_metric_per_sample_weighted = 22.0 / 10.0
+    expected_metric = np.sqrt(15.0 / 14.0)
+    expected_metric_weighted = np.sqrt(50.0 / 29.0)
+    expected_metric_per_sample = (np.sqrt(1.0) + np.sqrt(5.0)) / 4.0
+    expected_metric_per_sample_weighted = \
+        (2.0 * np.sqrt(1.0) + 4.0 * np.sqrt(5.0)) / 10.0
 
     def test_metric(self):
-        metric_obj = MeanSquaredErrorNaN()
+        metric_obj = RootMeanSquaredErrorNaN()
         metric = metric_obj(self.y_true, self.y_pred)
         self.assertAllClose(self.expected_metric, metric)
 
     def test_metric_weighted(self):
-        metric_obj = MeanSquaredErrorNaN()
+        metric_obj = RootMeanSquaredErrorNaN()
         metric = metric_obj(self.y_true, self.y_pred,
                             sample_weight=self.sample_weight)
         self.assertAllClose(self.expected_metric_weighted, metric)
 
     def test_metric_per_sample(self):
-        metric_obj = MeanSquaredErrorNaN(per_sample=True)
+        metric_obj = RootMeanSquaredErrorNaN(per_sample=True)
         metric = metric_obj(self.y_true, self.y_pred)
         self.assertAllClose(self.expected_metric_per_sample, metric)
 
     def test_metric_per_sample_weighted(self):
-        metric_obj = MeanSquaredErrorNaN(per_sample=True)
+        metric_obj = RootMeanSquaredErrorNaN(per_sample=True)
         metric = metric_obj(self.y_true, self.y_pred,
                             sample_weight=self.sample_weight)
         self.assertAllClose(self.expected_metric_per_sample_weighted, metric)
