cf67f4439930c1d55eada3aafc08e3e5a0a6eb12
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2019-12-18 16:44:34 +0000

    Remove NoDistributionStrategy unit tests

--- tests/tensorflow/distribute/test_noDistributionStrategy.py
+++
@@ -1,8 +0,0 @@
-from chemmltoolkit.tensorflow.distribute import NoDistributionStrategy
-
-
-class TestNoDistributionStrategy():
-    def test_scope(self):
-        strategy = NoDistributionStrategy()
-        with strategy.scope():
-            assert 1 == 1
