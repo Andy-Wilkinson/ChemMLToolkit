648d6bdc4c2ccf2f600df26e67691bbaa0e12de3
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2020-01-05 20:10:33 +0000

    Remove incomplete SmilesFeaturiser

--- chemmltoolkit/processing/__init__.py
+++ chemmltoolkit/processing/__init__.py
@@ -1,7 +1,5 @@
 from chemmltoolkit.processing.atomFeaturiser import AtomFeaturiser
 from chemmltoolkit.processing.bondFeaturiser import BondFeaturiser
 from chemmltoolkit.processing.moleculeFeaturiser import MoleculeFeaturiser
-from chemmltoolkit.processing.smilesFeaturiser import SmilesFeaturiser
 
-__all__ = ['AtomFeaturiser', 'BondFeaturiser', 'MoleculeFeaturiser',
-           'SmilesFeaturiser']
+__all__ = ['AtomFeaturiser', 'BondFeaturiser', 'MoleculeFeaturiser']
