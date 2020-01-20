2d1ae3712f67295b2e6f283c48af97e893b975fe
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2020-01-20 21:59:41 +0000

    Add spectral normalisation for TF

--- tests/tensorflow/processing/test_adjacency_ops_tf.py
+++ tests/tensorflow/processing/test_adjacency_ops_tf.py
@@ -54,3 +54,55 @@ class TestAdjacencyOpsTf(tf.test.TestCase):
 
         result = adjacency_ops.normalise(input)
         self.assertAllEqual(expected_result, result)
+
+    def test_normalise_spectral_rank3(self):
+        input = tf.convert_to_tensor(
+            [
+                [[0, 0, 1], [0, 0, 1], [1, 1, 0]],
+                [[1, 2, 2], [2, 3, 5], [2, 5, 3]]
+            ], dtype=tf.float32)
+
+        expected_result = tf.convert_to_tensor(
+            [
+                [[0, 0, 0.71], [0, 0, 0.71], [0.71, 0.71, 0]],
+                [[0.2, 0.28, 0.28], [0.28, 0.3, 0.5], [0.28, 0.5, 0.3]],
+            ], dtype=tf.float32)
+
+        result = adjacency_ops.normalise_spectral(input)
+        self.assertAllClose(expected_result, result, 0.02)
+
+    def test_normalise_spectral_rank4(self):
+        input = tf.convert_to_tensor(
+            [
+                [
+                    [[0, 0, 1], [0, 0, 1], [1, 1, 0]],
+                    [[1, 2, 2], [2, 3, 5], [2, 5, 3]]
+                ]
+            ], dtype=tf.float32)
+
+        expected_result = tf.convert_to_tensor(
+            [
+                [
+                    [[0, 0, 0.71], [0, 0, 0.71], [0.71, 0.71, 0]],
+                    [[0.2, 0.28, 0.28], [0.28, 0.3, 0.5], [0.28, 0.5, 0.3]],
+                ]
+            ], dtype=tf.float32)
+
+        result = adjacency_ops.normalise_spectral(input)
+        self.assertAllClose(expected_result, result, 0.02)
+
+    def test_normalise_spectral_zeros(self):
+        input = tf.convert_to_tensor(
+            [
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            ], dtype=tf.float32)
+
+        expected_result = tf.convert_to_tensor(
+            [
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            ], dtype=tf.float32)
+
+        result = adjacency_ops.normalise_spectral(input)
+        self.assertAllEqual(expected_result, result)
