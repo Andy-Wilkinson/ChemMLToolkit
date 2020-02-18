2e75322696538c8ce3402730c318e33c25f3c07b
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2020-02-18 19:47:35 +0000

    Allow arbitary RDKit descriptors for molecule features

--- tests/features/test_moleculeFeaturiser.py
+++ tests/features/test_moleculeFeaturiser.py
@@ -8,13 +8,16 @@ class TestMoleculeFeaturiser(object):
     @pytest.mark.parametrize("smiles_input,feature_fns,expected_output", [
         # Tests for individual features
         ('CCO', [mf.logp], [-0.0014000000000000123]),
+        ('CCO', [mf.rdkit('MolLogP')], [-0.0014000000000000123]),
         ('CCO', [mf.molwt], [46.069]),
+        ('CCO', [mf.rdkit('MolWt')], [46.069]),
         ('NCCO', [mf.num_h_donors], [2]),
         ('NCCO', [mf.num_h_acceptors], [2]),
         ('CCO', [mf.num_heavy_atoms], [3]),
         ('CCO', [mf.tpsa], [20.23]),
         # Tests for multiple features
         ('CCO', [mf.molwt, mf.tpsa, mf.num_heavy_atoms], [46.069, 20.23, 3]),
+        ('CCO', [mf.rdkit('MolWt'), mf.rdkit('TPSA')], [46.069, 20.23]),
     ])
     def test_process_molecule(self,
                               smiles_input,
@@ -49,3 +52,12 @@ class TestMoleculeFeaturiser(object):
         assert max(features) == mx
         assert min(features) == mn
         assert featuriser.get_feature_length() == length
+
+    def test_process_all_rdkit(self):
+        featuriser = MoleculeFeaturiser(mf.all_rdkit())
+        mol = Chem.MolFromSmiles('OCCCCO')
+        features = featuriser.process_molecule(mol)
+        assert len(features) == 200
+        assert features[0] == 8.08625
+        assert features[50] == 0.0
+        assert features[100] == 0
