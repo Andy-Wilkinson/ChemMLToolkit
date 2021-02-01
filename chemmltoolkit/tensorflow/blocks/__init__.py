e37e11a0dbd0dfd42e0d3fd17390f2f29571699b
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2021-02-01 21:27:03 +0000

    Remove TensorFlow predefined blocks

--- chemmltoolkit/tensorflow/blocks/__init__.py
+++
@@ -1,8 +0,0 @@
-from chemmltoolkit.tensorflow.blocks.conv1dBlock import Conv1DBlock
-from chemmltoolkit.tensorflow.blocks.denseBlock import DenseBlock
-from chemmltoolkit.tensorflow.blocks.dilConv1dWithGluBlock \
-    import DilConv1DWithGLUBlock
-from chemmltoolkit.tensorflow.blocks.recurrentBlock import RecurrentBlock
-
-__all__ = ['Conv1DBlock', 'DenseBlock', 'DilConv1DWithGLUBlock',
-           'RecurrentBlock']
