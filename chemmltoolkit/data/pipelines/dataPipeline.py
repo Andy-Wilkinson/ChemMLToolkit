2b867f6637eeda8a360652328aee327e26f1a04b
Andrew Wilkinson <Andy-Wilkinson@users.noreply.github.com>
2021-12-13 21:58:22 +0000

    Add validation splits in pipelines

--- chemmltoolkit/data/pipelines/dataPipeline.py
+++ chemmltoolkit/data/pipelines/dataPipeline.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Callable, Generic, Iterable, Iterator, Tuple, Type
+from typing import Callable, Generic, Iterable, Iterator, List, Tuple, Type, overload
 from typing import TypeVar, Union
 import itertools
 from sklearn.model_selection import train_test_split
@@ -21,20 +21,51 @@ class DataPipeline(Generic[T], Iterable[T]):
     def filter(self: DP, fn: Callable[[T], bool]) -> DP:
         return self.create(lambda: filter(fn, self))
 
+    @overload
     def train_test_split(self: DP,
-                         test_size: Union[None, float, int] = None,
-                         train_size: Union[None, float, int] = None,
+                         test_size: Union[float, int] = ...,
+                         val_size: None = ...,
+                         random_state: Union[None, int] = ...,
+                         shuffle: bool = ...
+                         ) -> Tuple[DP, DP]:
+        ...
+
+    @overload
+    def train_test_split(self: DP,
+                         test_size: Union[float, int] = ...,
+                         val_size: Union[float, int] = ...,
+                         random_state: Union[None, int] = ...,
+                         shuffle: bool = ...
+                         ) -> Tuple[DP, DP, DP]:
+        ...
+
+    def train_test_split(self: DP,
+                         test_size: Union[float, int] = 0.25,
+                         val_size: Union[None, float, int] = None,
                          random_state: Union[None, int] = None,
                          shuffle: bool = True
-                         ) -> Tuple[DP, DP]:
-        vals = list(iter(self))
-        train, test = train_test_split(vals,
+                         ) -> Union[Tuple[DP, DP], Tuple[DP, DP, DP]]:
+        all: List[T] = list(iter(self))
+
+        train, test = train_test_split(all,
                                        test_size=test_size,
-                                       train_size=train_size,
                                        random_state=random_state,
                                        shuffle=shuffle)
+        if val_size is None:
+            return (self.create(lambda: iter(train)),
+                    self.create(lambda: iter(test)))
+
+        random_state = random_state + 1 if random_state else None
+        if isinstance(val_size, float):
+            val_size = int(len(all) * val_size)
+
+        train, val = train_test_split(train,
+                                      test_size=val_size,
+                                      random_state=random_state,
+                                      shuffle=shuffle)
 
         return (self.create(lambda: iter(train)),
+                self.create(lambda: iter(val)),
                 self.create(lambda: iter(test)))
 
     @ classmethod
